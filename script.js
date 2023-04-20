b9be050b53bb3ea84790bf096465121224403fb3
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2023-04-21 00:51:55 +0100

    New commit

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 //ONE
 let body = document.body
+
+//Get all elements in document body by tag name
 let elements = body.getElementsByTagName('*')
 let elementsArray = Object.values(elements)
 
-let tagArray = []
+//Function to search for a tag with a specific value
+function checkTag(tag, value) {
+    let tagArray = []
 
-function checkTag(tag) {
     for (i = 0; i < elementsArray.length; i++) {
-        if (elementsArray[i].localName == tag) {
+        if (elementsArray[i].localName == tag && elementsArray[i].innerHTML == value) {
             tagArray.push(elementsArray[i])
         }
     }
 
-    return tagArray
+    if (tagArray.length == 0) {
+        return false
+    } else return true
 }
 
+//Testing 
+console.log('Check h1 Tag with value "HELLO WORLD": ', checkTag('h1', 'HELLO WORLD'))
+console.log('Check h1 Tag with value "Hello": ', checkTag('h1', 'Hello'))
 
-console.log('Check Tag: ', checkTag('h1'))
-
-console.log('Array', elementsArray)
 
 
 
@@ -29,7 +34,7 @@ let multiDimensionalArray = [
     [1, 2],
     ['car'],
     [3, 4, 5],
-    [6, 7]
+    [6, 7, 8]
 ]
 
 let flattenedArray = [].concat.apply([], multiDimensionalArray);
@@ -49,5 +54,6 @@ for (i = 0; i < flattenedArray.length; i++) {
     }
 }
 
+//Testing
 console.log('No of integers: ', integersCount)
 console.log('New integer array: ', newIntegerArray)
